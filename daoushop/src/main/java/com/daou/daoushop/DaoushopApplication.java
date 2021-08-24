2924c5bc26a0efc55ec17af825bbefcfdf506198
Seokho Lee <dldlsh2309@gmail.com>
2021-08-24 14:37:56 +0900

    ER-Diagram 추가 및프로젝트 생성

---
+++ daoushop/src/main/java/com/daou/daoushop/DaoushopApplication.java
@@ -0,0 +1,13 @@
+package com.daou.daoushop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoushopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaoushopApplication.class, args);
+	}
+
+}
---
+++ daoushop/src/test/java/com/daou/daoushop/DaoushopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daou.daoushop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoushopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
