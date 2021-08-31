9857baf51645429d920258291583851d27a3f253
Seokho Lee <dldlsh2309@gmail.com>
2021-08-31 17:15:19 +0900

    fix : 유저 보유금 entity 수정

--- daoushop/src/main/java/com/daou/daoushop/domain/userMoney/UserMoneyEntity.java
+++ daoushop/src/main/java/com/daou/daoushop/domain/userMoney/UserMoneyEntity.java
@@ -1,8 +1,14 @@
 package com.daou.daoushop.domain.userMoney;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import com.daou.daoushop.domain.user.UserEntity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,9 +16,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 @Entity(name="user_money")
-public class UserMoneyEntity {
+public class UserMoneyEntity implements Serializable {
 	
 	@Id
-	@GeneratedValue
-	private Integer userMoneyId;
+	@OneToOne
+	@JoinColumn(name="user_number")
+	private UserEntity user;
+	private int fund;
+	
+	public UserMoneyEntity(UserEntity user, int fund) {
+		this.user = user;
+		this.fund = fund;
+	}
+	
 }
--- daoushop/src/main/java/com/daou/daoushop/web/OrderController.java
+++ daoushop/src/main/java/com/daou/daoushop/web/OrderController.java
@@ -1,5 +1,25 @@
 package com.daou.daoushop.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.daou.daoushop.service.OrderService;
+import com.daou.daoushop.web.dto.OrderRequestDto;
+import com.daou.daoushop.web.dto.OrderResponseDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/order")
 public class OrderController {
 
+//	private final OrderService orderService;
+//	
+//	@GetMapping(value = "/getAutoPay")
+//	public OrderResponseDto findAutoPayInfo(@RequestBody OrderRequestDto requestDto) {
+//		return orderService.findAutoPayInfo(requestDto);
+//	}
 }
