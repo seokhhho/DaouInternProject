a1ecc4eb50c0edf5c705f4e9e09620e76187922f
Seokho Lee <dldlsh2309@gmail.com>
2021-09-10 09:28:20 +0900

    refactor :orderService 리팩토링 작업
    fix : not null 추가

--- daoushop/src/main/java/com/daou/daoushop/service/OrderService.java
+++ daoushop/src/main/java/com/daou/daoushop/service/OrderService.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -60,7 +61,7 @@ public class OrderService {
 	
 	/*자동 결제 시 결제 정보 가져오기*/
 	@Transactional
-	public OrderResponseDto findAutoPayInfo(OrderRequestDto requestDto) {
+	public OrderResponseDto findAutoPayInfo(@NotNull OrderRequestDto requestDto) {
 		
 		List<ProductAmountDto> buyedProducts = requestDto.getProducts();
 		
@@ -73,77 +74,25 @@ public class OrderService {
 		int pgPayMoney = 0;
 		
 		AutoPayInfo autoPayInfo = new AutoPayInfo(0,0,null,new ArrayList<>(),0,0);
-		
+
 		/*상품 정보를 통해 총 가격 구하기*/
-		for(ProductAmountDto p : buyedProducts) {
-			ProductEntity product = productRepository.findById(p.getProductId())
-					.orElseThrow(() -> new IllegalArgumentException( p.getProductId() +"번 상품이 존재 하지 않습니다."));
-			if(product.getStock() < p.getAmount()) {
-				throw new IllegalArgumentException("재고가 부족합니다.");
-			}
-			totalPrice += product.getPrice() * p.getAmount();
-//			autoPayInfo.setTotalPrice(autoPayInfo.getTotalPrice() + (product.getPrice() * p.getAmount()));
-		}
-		
+		totalPrice = getTotalPrice(buyedProducts, totalPrice);
+
 		/*유저 정보 불러오기*/
 		UserEntity user = userRepository.findById(requestDto.getUserNumber())
 					.orElseThrow(() -> new IllegalArgumentException( "해당 유저가 존재 하지 않습니다."));
-		
-		
+
 		/*쿠폰 정보 불러와 가장 적합한 쿠폰 선택 후 할인 가격 적용*/
-		Set<CouponEntity> coupons = user.getCoupons();
-		int discountRate = 0;
-		
-		for(CouponEntity c:coupons) {
-			if(!c.getIsUsed().isUsed() && c.getDiscountRate().getMinPrice() <= totalPrice 
-					&& discountRate < c.getDiscountRate().getRate()) { 
-				usingCoupon = c;
-				discountRate = c.getDiscountRate().getRate();
-			}
-		}
-		
+		usingCoupon = getCouponEntity(totalPrice, usingCoupon, user);
+
 		discountedPrice = totalPrice;
-		
 		if(usingCoupon != null) {
 			discountedPrice = (int)(totalPrice - ((float)totalPrice / 100 * usingCoupon.getDiscountRate().getRate()));
 		}
-		
-		
+
 		/*사용 할 포인트 적용*/
-		List<PointEntity> points = null;
-		points = new ArrayList(user.getPoints());
-		Collections.sort(points);
-		
-		int remainMoney = discountedPrice;
-		
-		Calendar calendar = Calendar.getInstance();
-		Date validDay = calendar.getTime();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		String now = sdf.format(validDay);
-		
-		for(PointEntity p : points) {
-			if(p.getPointMoney() > 0 && p.getValid().compareTo(now) > 0) {
-				if(p.getPointMoney() >= remainMoney) {
-					usingPoints.add(UsingPointDto.builder()
-							.pointId(p.getPointId())
-							.valid(p.getValid())
-							.usingMoney(remainMoney)
-							.pointName(p.getPointName())
-							.build());
-					remainMoney = 0;
-					break;
-				}else {
-					usingPoints.add(UsingPointDto.builder()
-							.pointId(p.getPointId())
-							.valid(p.getValid())
-							.usingMoney(p.getPointMoney())
-							.pointName(p.getPointName())
-							.build());
-					remainMoney -= p.getPointMoney();
-				}
-			}
-		}
-		
+		int remainMoney = getRemainMoney(discountedPrice, usingPoints, user);
+
 		/*남은 돈 있을 시 적립금 적용*/
 		if(remainMoney > 0) {
 			if(user.getUserMoney().getFund() >= remainMoney) {
@@ -181,8 +130,72 @@ public class OrderService {
 		}
 	}
 
+	public int getTotalPrice(@org.jetbrains.annotations.NotNull List<ProductAmountDto> buyedProducts, int totalPrice) {
+		for(ProductAmountDto p : buyedProducts) {
+			ProductEntity product = productRepository.findById(p.getProductId())
+					.orElseThrow(() -> new IllegalArgumentException( p.getProductId() +"번 상품이 존재 하지 않습니다."));
+			if(product.getStock() < p.getAmount()) {
+				throw new IllegalArgumentException("재고가 부족합니다.");
+			}
+			totalPrice += product.getPrice() * p.getAmount();
+		}
+		return totalPrice;
+	}
+
+	private int getRemainMoney(int discountedPrice, List<UsingPointDto> usingPoints, @NotNull UserEntity user) {
+		List<PointEntity> points = null;
+		points = new ArrayList(user.getPoints());git
+		Collections.sort(points);
+
+		int remainMoney = discountedPrice;
+
+		Calendar calendar = Calendar.getInstance();
+		Date validDay = calendar.getTime();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String now = sdf.format(validDay);
+
+		for(PointEntity p : points) {
+			if(p.getPointMoney() > 0 && p.getValid().compareTo(now) > 0) {
+				if(p.getPointMoney() >= remainMoney) {
+					usingPoints.add(UsingPointDto.builder()
+							.pointId(p.getPointId())
+							.valid(p.getValid())
+							.usingMoney(remainMoney)
+							.pointName(p.getPointName())
+							.build());
+					remainMoney = 0;
+					break;
+				}else {
+					usingPoints.add(UsingPointDto.builder()
+							.pointId(p.getPointId())
+							.valid(p.getValid())
+							.usingMoney(p.getPointMoney())
+							.pointName(p.getPointName())
+							.build());
+					remainMoney -= p.getPointMoney();
+				}
+			}
+		}
+		return remainMoney;
+	}
+
+
+	private CouponEntity getCouponEntity(int totalPrice, CouponEntity usingCoupon, @NotNull UserEntity user) {
+		Set<CouponEntity> coupons = user.getCoupons();
+		int discountRate = 0;
+
+		for(CouponEntity c:coupons) {
+			if(!c.getIsUsed().isUsed() && c.getDiscountRate().getMinPrice() <= totalPrice
+					&& discountRate < c.getDiscountRate().getRate()) {
+				usingCoupon = c;
+				discountRate = c.getDiscountRate().getRate();
+			}
+		}
+		return usingCoupon;
+	}
+
 	@Transactional
-	public OrderBalanceResponseDto autoPay(PayRequestDto requestDto) {
+	public OrderBalanceResponseDto autoPay(@org.jetbrains.annotations.NotNull PayRequestDto requestDto) {
 		
 		UserEntity user = userRepository.findById(requestDto.getUserNumber())
 				.orElseThrow(() -> new IllegalArgumentException( "해당 유저가 존재 하지 않습니다."));
--- daoushop/src/main/java/com/daou/daoushop/service/ProductService.java
+++ daoushop/src/main/java/com/daou/daoushop/service/ProductService.java
@@ -3,6 +3,8 @@ package com.daou.daoushop.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.daou.daoushop.domain.product.ProductEntity;
+import com.daou.daoushop.web.dto.ProductAmountDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,4 +25,6 @@ public class ProductService {
 				.map(ProductResponseDto::new)
 				.collect(Collectors.toList());
 	}
+
+
 }
